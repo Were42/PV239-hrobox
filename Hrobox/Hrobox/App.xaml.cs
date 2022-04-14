5251c16b62a92cffeb9bb53bdfd1987bcc2b4818
Were42 <xkoudel4@fi.muni.cz>
2022-04-14 18:37:25 +0200

    repair namespace

--- Hrobox/Hrobox/App.xaml.cs
+++ Hrobox/Hrobox/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Hrobox.View;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
--- Hrobox/Hrobox/View/MainPage.xaml.cs
+++ Hrobox/Hrobox/View/MainPage.xaml.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
-namespace Hrobox
+namespace Hrobox.View
 {
     public partial class MainPage : ContentPage
     {
