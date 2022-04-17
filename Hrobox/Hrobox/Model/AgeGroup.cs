fca837a86ff1b814cf724b16e06afe75999e1c04
Karolina42 <karolina.vesela98@seznam.cz>
2022-04-17 14:41:35 +0200

    models

--- Hrobox/Hrobox/Model/AgeGroup.cs
+++ Hrobox/Hrobox/Model/AgeGroup.cs
@@ -2,5 +2,10 @@
 {
     public class AgeGroup
     {
+        enum Age
+        {
+            
+
+        }
     }
 }
--- Hrobox/Hrobox/Model/GameModel.cs
+++ Hrobox/Hrobox/Model/GameModel.cs
@@ -6,5 +6,13 @@ namespace Hrobox.Model
 {
     internal class GameModel
     {
+        public string Name { get; set; }
+        public string Rules { get; set; }
+        public List<string> Tags { get; set; }
+        public Duration Duration { get; set; }
+        public string Version { get; set; }
+        public int MinNumPlayers { get; set; }
+        public int MaxNumPlayers { get; set; }
+        public AgeGroup AgeGroups { get; set; }
     }
 }
