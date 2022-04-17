dc994617a3c66322d6ba6d03794a5414efa9fd0c
Were42 <xkoudel4@fi.muni.cz>
2022-04-17 21:56:23 +0200

    GameViewModel with command usage

---
+++ Hrobox/Hrobox/ViewModel/GamesViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Hrobox.Model;
+using IOS_BP_APP.Models;
+
+namespace Hrobox.ViewModel
+{
+    internal class GamesViewModel : ViewModelBase
+    {
+        public ObservableCollection<GameModel> Games { get; set; }
+        public string KeyWord { get; set; } = "";
+        private ICommand find;
+        public ICommand Find { get { return find; } }
+
+        public GamesViewModel()
+        {
+            find = new DelegateCommand(FindIt);
+        }
+        public void FindIt()
+        {
+            Console.WriteLine("HI");
+        }
+    }
+}
