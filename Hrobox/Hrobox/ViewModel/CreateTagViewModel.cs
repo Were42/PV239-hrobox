7c41686431bc07a2662145f6cfedadc644cda4c4
Karolina42 <karolina.vesela98@seznam.cz>
2022-04-18 17:03:08 +0200

    added command to create tag view

--- Hrobox/Hrobox/ViewModel/CreateTagViewModel.cs
+++ Hrobox/Hrobox/ViewModel/CreateTagViewModel.cs
@@ -1,14 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 using Hrobox.Model;
+using IOS_BP_APP.Models;
 
 namespace Hrobox.ViewModel
 {
     internal class CreateTagViewModel : ViewModelBase
     {
-        public Tag Tag { get; set; }
-    }
+        public string Tag { get; set; }
 
+        public ICommand createTag;
+        public ICommand CreateTag { get; }
+
+        public CreateTagViewModel()
+        {
+            this.createTag = new DelegateCommand(CreateTagCommand);
+        }
+
+        private void CreateTagCommand()
+        {
+            throw new NotImplementedException();
+        }
+    }
 
 }
