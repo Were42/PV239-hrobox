dd5b26b36ee1ce62226259490d06a3f5b8b795a5
Karolina42 <karolina.vesela98@seznam.cz>
2022-04-19 19:05:07 +0200

    new ViewModels

---
+++ Hrobox/Hrobox/ViewModel/LoginViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Hrobox.Model;
+using IOS_BP_APP.Models;
+using Xamarin.Forms;
+
+namespace Hrobox.ViewModel
+{
+    internal class LoginViewModel
+    {
+        public UserModel User { get; set; }
+        private ICommand loginCommand;
+        public ICommand LoginCommand { get; }
+
+        public LoginViewModel()
+        {
+            this.User = new UserModel();
+            this.loginCommand = new DelegateCommand(Login);
+        }
+
+        private void Login()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Hrobox/Hrobox/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Hrobox.Model;
+using IOS_BP_APP.Models;
+
+namespace Hrobox.ViewModel
+{
+    internal class RegisterViewModel
+    {
+        public UserModel User { get; set; }
+
+        private ICommand registerCommand;
+
+        public ICommand RegisterCommand { get; }
+
+        public RegisterViewModel()
+        {
+            this.User = new UserModel();
+            registerCommand = new DelegateCommand(Register);
+        }
+
+        private void Register()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
