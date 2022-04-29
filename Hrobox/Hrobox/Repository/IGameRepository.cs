9f5626ace98dced74ff05da41b2d703f89d9e362
Karolina42 <karolina.vesela98@seznam.cz>
2022-04-29 17:55:22 +0200

    Create IGameRepository.cs

---
+++ Hrobox/Hrobox/Repository/IGameRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Hrobox.Model;
+
+namespace Hrobox.Repository
+{
+    public interface IGameRepository
+    {
+        Task<List<GameModel>> GetAll();
+        Task CreateGame(GameModel game, bool isNewItem);
+        Task UpdateGame(GameModel game, bool isNewItem);
+        Task deleteGame(string id);
+
+    }
+}
