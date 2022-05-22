941b8b9c7c95f0bad731f089d8ca015d5c8dd70a
Karolina42 <karolina.vesela98@seznam.cz>
2022-05-22 08:19:06 +0200

    string format in tag repo

--- Hrobox/Hrobox/Repository/TagRestRepository.cs
+++ Hrobox/Hrobox/Repository/TagRestRepository.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Hrobox.Model;
+using Hrobox.Utility;
 
 namespace Hrobox.Repository
 {
@@ -19,7 +20,7 @@ namespace Hrobox.Repository
         }
         public async Task<TagsModel> GetAllTags()
         {
-            Uri uri = new Uri("https://hrobox-backend.herokuapp.com/api/tags");
+            Uri uri = new Uri(String.Format("{0}{1}", Constants.Url, "tags"));
             HttpResponseMessage response = await client.GetAsync(uri);
             TagsModel Items = new TagsModel();
             if (response.IsSuccessStatusCode)
@@ -35,7 +36,7 @@ namespace Hrobox.Repository
         {
             var authHeader = new AuthenticationHeaderValue("Bearer", jwt);
             client.DefaultRequestHeaders.Authorization = authHeader;
-            Uri uri = new Uri("https://hrobox-backend.herokuapp.com/api/tag");
+            Uri uri = new Uri(String.Format("{0}{1}", Constants.Url, "tag"));
             string json = JsonSerializer.Serialize<Tag>(tag, new JsonSerializerOptions { IgnoreNullValues = true, PropertyNamingPolicy = JsonNamingPolicy.CamelCase});
             StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
 
